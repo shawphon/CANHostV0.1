6133f1c7119a03292f19ca11ba819140649d8794
xiaofengzhang <shawphon@163.com>
2020-06-29 15:48:15 +0800

    测试develop

--- ZHISIGHT/ConfigFileForm.Designer.cs
+++ ZHISIGHT/ConfigFileForm.Designer.cs
@@ -142,6 +142,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ConfigFileForm";
             this.Text = "ConfigFileForm";
+            this.Load += new System.EventHandler(this.ConfigFileForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
